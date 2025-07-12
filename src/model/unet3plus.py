cab9388e64b3dedc20ab107b82e00a217bd4058b
Taha-bouhafa1 <tahabouhafa1@gmail.com>
2025-07-12 15:47:52 +0100

    update unet3plus

--- src/model/unet3plus.py
+++ src/model/unet3plus.py
@@ -58,6 +58,8 @@ class UNet3Plus(nn.Module):
 
         self.d4=conv_block(64*5,64)
 
+        """ DECODER 3 """
+        self
 
 
     def forward(self,inputs):
@@ -74,6 +76,20 @@ class UNet3Plus(nn.Module):
 
         e1_d4=F.max_pool2d(e1,kernel_size=8,stride=8)
         e1_d4=self.e1_d4(e1_d4)
+
+        e2_d4=F.max_pool2d(e2,kernel_size=4,stride=4)
+        e2_d4=self.e2_d4(e2_d4)
+
+        e3_d4=F.max_pool2d(e3,kernel_size=2,stride=2)
+        e3_d4=self.e3_d4(e3_d4)
+
+        e4_d4=self.e4_d4(e4)
+
+        e5_d4=F.interpolate(e5,scale_factor=2,mode="bilinear",align_corners=True)
+        e5_d4=self.e5_d4(e5_d4)
+
+        d4=torch.cat([e1_d4,e2_d4,e3_d4,e4_d4,e5_d4],dim=1)
+        d4=self.d4(d4)
 
 
 
